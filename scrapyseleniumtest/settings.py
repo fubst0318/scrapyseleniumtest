8edfb747b742713475721c0153169945f6c5e482
fubst0318 <fubst0318@126.com>
2018-10-20 17:23:11 +0800

    taobao ipad 抓取 first version

--- scrapyseleniumtest/settings.py
+++ scrapyseleniumtest/settings.py
@@ -39,34 +39,34 @@ ROBOTSTXT_OBEY = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'scrapyseleniumtest.middlewares.ScrapyseleniumtestSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapyseleniumtest.middlewares.SeleniumMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'scrapyseleniumtest.middlewares.ScrapyseleniumtestDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapyseleniumtest.pipelines.ScrapyseleniumtestPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'scrapyseleniumtest.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -92,3 +92,9 @@ ROBOTSTXT_OBEY = False
 
 KEYWORDS = ['iPad']
 MAX_PAGE = 100
+
+SELENIUM_TIMEOUT = 20
+PHANTOMJS_SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
+
+MONGO_URI = 'localhost'
+MONGO_DB = 'tb_new'
